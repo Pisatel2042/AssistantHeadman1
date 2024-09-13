0e607b68ca3d2df6be87bc7c206221156cb83ce0
Pisatel2042 <shumeyko.vasya@bk.ru>
2024-09-13 20:23:02 +0300

    FirstVersion

---
+++ AssistantHeadman/AssistantHeadman/LoginPage.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Essentials;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace AssistantHeadman
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class LoginPage : ContentPage
+    {
+
+        
+        public LoginPage()
+        {
+            InitializeComponent();
+        }
+        protected override void OnAppearing()
+        {
+           //dgfhdhdhg
+        }
+        private async void GoToMain(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new MainPage());
+        }
+    }
+}
